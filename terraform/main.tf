3ae95c735bba91f600f78b6738f1fc1491444a0b
mstaylor <bud.staylor@gmail.com>
2026-04-01 17:54:57 -0400

    adds terraform to simplify deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -61,7 +61,7 @@ resource "aws_ecs_task_definition" "rendezvous" {
 
   runtime_platform {
     operating_system_family = "LINUX"
-    cpu_architecture        = "ARM64"
+    cpu_architecture        = "X86_64"
   }
 
   container_definitions = jsonencode([
