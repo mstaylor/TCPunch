15cb11edea18089271f1dcf3def2541871d2b4ae
mstaylor <bud.staylor@gmail.com>
2026-04-01 17:36:37 -0400

    adds terraform to simplify deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -59,6 +59,11 @@ resource "aws_ecs_task_definition" "rendezvous" {
   memory                   = var.task_memory
   execution_role_arn       = aws_iam_role.ecs_execution.arn
 
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture        = "ARM64"
+  }
+
   container_definitions = jsonencode([
     {
       name      = var.name
